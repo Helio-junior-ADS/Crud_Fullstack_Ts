837b0d8bff08cf105c221ddd380f6de9cc2806c8
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-11-16 18:34:33 -0300

    config inicial
    Install dependencias

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from './index';
+import {config} from 'dotenv';
+config();
+
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, ()=> {
+    console.log(`Server ONLINE no PATH http://localhost:${PORT}`);
+});
+
